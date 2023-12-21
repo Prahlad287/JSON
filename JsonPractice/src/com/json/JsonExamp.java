c1ca7e7f9b4bbada9637d65587069421c08d2f02
DELL <DELL@DESKTOP-TK9LMLV>
2023-12-21 17:26:33 +0530

    new

--- JsonPractice/src/com/json/JsonExamp.java
+++ JsonPractice/src/com/json/JsonExamp.java
@@ -15,17 +15,36 @@ import org.json.simple.parser.JSONParser;
 public class JsonExamp{
 
 	public static void main(String[] args) throws IOException {
-		FileReader reader=new FileReader("C:\\Users\\DELL\\eclipse-workspace\\JsonPractice\\Test.JSON");
+		FileReader reader=new FileReader("D:\\Java Notes\\Test.json");
 	    JSONObject obj=(JSONObject)JSONValue.parse(reader);
 	    System.out.println(obj);
+	    System.out.println();
 	    
-	    FileReader reader2=new FileReader("C:\\Users\\DELL\\eclipse-workspace\\JsonPractice\\Test2.json");
-	    JSONArray array=(JSONArray)JSONValue.parse(reader2);
-	    System.out.println(array);
-	    System.out.println(array.size());
-	    JSONObject obj2=(JSONObject)array.get(0);
-	    System.out.println(obj2);
-	    System.out.println(obj2.get("name"));
+	    JSONArray obj1=(JSONArray)obj.get("courses");
+	    System.out.println(obj1);
+	    System.out.println(obj1.get(1));
+	    System.out.println();
+	    
+	    System.out.println(obj.get("address"));
+	    JSONObject obj3=(JSONObject)obj.get("address");
+	    System.out.println(obj3.get("mob"));
+	    System.out.println();
+	    
+	    System.out.println(obj.get("name"));
+	    System.out.println(obj.get("id"));
+	    System.out.println(obj.get("email"));
+	    
+	    
+	    
+	    
+//	    System.out.println();
+//	    FileReader reader2=new FileReader("D:\\Java Notes\\Test2.json");
+//	    JSONArray array=(JSONArray)JSONValue.parse(reader2);
+//	    System.out.println(array);
+//	    System.out.println(array.size());
+//	    JSONObject obj2=(JSONObject)array.get(0);
+//	    System.out.println(obj2);
+//	    System.out.println(obj2.get("name"));
 		
 		
 
